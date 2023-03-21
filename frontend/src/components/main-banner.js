93cdb48f6dd3df874ac521cd7dbb8db3b4168e9b
Zwirek_Dev <glanzgarage@gmail.com>
2023-03-21 07:19:29 +0100

    first commit

---
+++ frontend/src/components/main-banner.js
@@ -0,0 +1,27 @@
+import React from "react";
+import CateringIcon from "../images/catering.png";
+
+export const MainBanner = () => {
+  return (
+    <div className="main-banner main-banner--yellow-mandarine">
+      <div className="main-banner__logo">
+        <img
+          className="main-banner__image"
+          src={CateringIcon}
+          alt="Catering Icon"
+        />
+      </div>
+      <h1 className="main-banner__headline">Witamy w Catering Manager</h1>
+      <p className="main-banner__description">
+        Ta aplikacja pomoże Ci utrzymać aktualny stan magazynu. Jednocześnie
+        ułatwi i przyspieszy tworzenie list zakupowych.
+      </p>
+      <a
+        href="https://www.flaticon.com/free-icons/catering"
+        title="catering icons"
+      >
+        Catering icons created by Freepik - Flaticon
+      </a>
+    </div>
+  );
+};
---
+++ frontend/src/pages/storage-page.js
@@ -0,0 +1,57 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React, { useEffect, useState } from "react";
+import { Table } from "../components/table";
+import { PageLayout } from "../components/page-layout";
+import { getProtectedResource } from "../services/message.service";
+
+export const ProtectedPage = () => {
+  const [message, setMessage] = useState("");
+
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const getMessage = async () => {
+      const accessToken = await getAccessTokenSilently();
+      const { data, error } = await getProtectedResource(accessToken);
+
+      if (!isMounted) {
+        return;
+      }
+
+      if (data) {
+        setMessage(JSON.stringify(data, null, 2));
+      }
+
+      if (error) {
+        setMessage(JSON.stringify(error, null, 2));
+      }
+    };
+
+    getMessage();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [getAccessTokenSilently]);
+
+  return (
+    <PageLayout>
+      <div className="content-layout">
+        <h1 id="page-title" className="content__title">
+          Magazyn
+        </h1>
+        <div className="content__body">
+          <p id="page-description">
+            <span>
+              Tutaj możesz sprawdzić swój stan magazynowy oraz zrobić
+              inwentaryzacje.
+            </span>
+          </p>
+          <Table title="Protected Message" code={message} />
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
